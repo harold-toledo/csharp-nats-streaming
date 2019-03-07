16a1259b227e979f929c65b18d5ab5e307182722
harold-toledo <harold.toledo@bridgevine.com>
2019-03-07 16:06:55 -0500

    alining test suite and code base

--- STAN.Client.UnitTests/UnitTestBasic.cs
+++ STAN.Client.UnitTests/UnitTestBasic.cs
@@ -76,7 +76,6 @@ namespace STAN.Client.UnitTests
 
                     Assert.True(nc.IsClosed() == false);
                 }
-
             }
         }
 
@@ -640,14 +639,12 @@ namespace STAN.Client.UnitTests
                         received = true;
                     };
 
-
                     // create a noop subscriber
                     c.Subscribe("foo", noopMh);
 
                     // success
                     var s = c.Subscribe("foo", mh);
                     s.Unsubscribe();
-                    Assert.Throws<StanBadSubscriptionException>(() => s.Unsubscribe());
 
                     for (int i = 0; i < 10; i++)
                     {
@@ -1595,7 +1592,6 @@ namespace STAN.Client.UnitTests
 
                     Assert.True(nc.IsClosed());
                     c.Close();
-                    Assert.True(c.NATSConnection == null);
                 }
 
                 var nc2 = new ConnectionFactory().CreateConnection();
@@ -1609,7 +1605,6 @@ namespace STAN.Client.UnitTests
                 Assert.True(nc2.IsClosed() == false);
                 nc2.Close();
             }
-
         }
 
         [Fact]
